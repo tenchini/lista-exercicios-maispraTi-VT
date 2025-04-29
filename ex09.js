fbb2ead8d742e1f268fe65d21ebe3694c4b125f9
tenchini <tenchini.dev@gmail.com>
2025-04-29 16:46:42 -0300

    resolução do exercício 09

--- ex09.js
+++ ex09.js
@@ -1,2 +1,13 @@
 // Implemente um programa que exibe uma contagem regressiva de 10 até 1 no console
 // utilizando um loop for.
+
+function countdown() {
+    const numeros = [];
+    for (let i = 10; i > 0; i--) {
+        numeros.push(i);
+    }
+
+    console.log(numeros.join(', ') + '.');
+}
+
+countdown();

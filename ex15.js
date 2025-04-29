084b2e646c1d31eb5cc83e6a7bdcc99ca887e54b
tenchini <tenchini.dev@gmail.com>
2025-04-29 17:30:35 -0300

    resolução do exercício 15

--- ex15.js
+++ ex15.js
@@ -1,2 +1,12 @@
 // Escreva um programa que gera e imprime os primeiros 10 números da sequência de
 // Fibonacci utilizando um loop for.
+
+function getFib(n) {
+    let fib = [0, 1];
+    for (let i = 2; i <= n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    console.log(fib.join(', ') + '.');
+}
+
+getFib(10);

26cde86dc55ea9870471933064204a03fe49b926
tenchini <tenchini.dev@gmail.com>
2025-04-29 15:08:04 -0300

    resolução do exercício 01

--- ex01.js
+++ ex01.js
@@ -1,2 +1,18 @@
 // Escreva um programa que recebe um número inteiro e verifica se ele é par ou ímpar
 // utilizando uma estrutura de controle if.
+
+function isEvenOrOdd(number) {
+    if (isNaN(number) || !Number.isInteger(number)) {
+        console.log('Por favor, digite um número inteiro válido.');
+        return;
+    }
+    number % 2 === 0
+        ? console.log(`O número ${number} é par!`)
+        : console.log(`O número ${number} é ímpar!`);
+}
+
+isEvenOrOdd(14);
+isEvenOrOdd(7);
+isEvenOrOdd('a');
+isEvenOrOdd(1.3);
+isEvenOrOdd(-2);

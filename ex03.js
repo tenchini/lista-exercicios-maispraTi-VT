f5f410bae06ed69017ce7a0520569d2b2da63b4e
tenchini <tenchini.dev@gmail.com>
2025-04-29 15:08:33 -0300

    resolução do exercício 03

--- ex03.js
+++ ex03.js
@@ -1,2 +1,25 @@
 // Implemente um programa que recebe uma nota de 0 a 10 e classifica como
 // "Aprovado", "Recuperação", ou "Reprovado" utilizando if-else if.
+
+function getStudentStatus(nota) {
+    if (isNaN(nota) || nota > 10 || nota < 0) {
+        console.log(
+            'Por favor, verifique a nota do aluno. A nota precisa ser um número positivo.'
+        );
+        return;
+    }
+    if (nota >= 7) {
+        console.log('Aprovado');
+    } else if (nota >= 5) {
+        console.log('Recuperação');
+    } else {
+        console.log('Reprovado');
+    }
+}
+
+getStudentStatus(3.5);
+getStudentStatus(6.7);
+getStudentStatus(9.8);
+getStudentStatus('a');
+getStudentStatus(-1);
+getStudentStatus(11);

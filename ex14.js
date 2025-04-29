146137d0cf4b13870c6eb50cf3a50de6eb0fb779
tenchini <tenchini.dev@gmail.com>
2025-04-29 17:30:21 -0300

    resolução do exercício 14

--- ex14.js
+++ ex14.js
@@ -1,2 +1,31 @@
 // Crie um programa que calcula o fatorial de um número fornecido pelo usuário
 // utilizando um loop for ou while.
+
+const prompt = require('prompt-sync')();
+
+function getFactorial() {
+    let fat;
+    while (true) {
+        let entrada = prompt('Digite um valor para ter o fatorial: ');
+        fat = Number(entrada);
+
+        if (isNaN(fat) || !Number.isInteger(fat) || fat < 0) {
+            console.log('Por favor, digite um número inteiro positivo.');
+        } else {
+            let total = 1;
+            let expressao = '';
+            for (let i = fat; i > 0; i--) {
+                total *= i;
+                expressao += i;
+
+                if (i > 1) {
+                    expressao += ' x ';
+                }
+            }
+            console.log(`${fat}! = ${expressao} = ${total}`);
+            break;
+        }
+    }
+}
+
+getFactorial();

5296be952a776eee5ee8847533d600c02f96c104
tenchini <tenchini.dev@gmail.com>
2025-05-08 17:03:35 -0300

    refactor: refatora ex04 delegando funções e evitando repetição

--- ex04.js
+++ ex04.js
@@ -1,39 +1,62 @@
 // Crie um menu interativo no console que oferece ao usuário a escolha de três opções.
 // Utilize switch-case para implementar a lógica de cada opção selecionada.
 
-let prompt = require('prompt-sync')();
+let prompt = require('prompt-sync')()
+const { shouldContinue } = require('./askToContinue')
 
-function veryUsefulSystem() {
-    console.log(
-        'Bem-vindo ao sistema que não faz nada, escolha sua opção de forma sábia!\n'
-    );
-    console.log(
-        "[1] Não fazer nada e fechar\n[2] Imprimir um número aleatório de 0 a 10\n[3] Ler 'Obrigado por usar o sistema' e encerrar a execução"
-    );
+function getValidnumber(message) {
+    while (true) {
+        const input = prompt(message)
+        const number = Number(input)
 
-    let opcao = Number(prompt('O que deseja fazer?  '));
-
-    switch (opcao) {
-        case 1:
-            break;
-        case 2:
-            const number = Math.floor(Math.random() * 11),
-                euMenti = 0;
-            if (euMenti === 0) {
-                console.log(`Ainda é azarado bicho, tu tirou ${euMenti}!!!!`);
-            } else {
-                console.log(number);
-            }
-            break;
-        case 3:
-            console.log('Obrigado por usar o sistema');
-            break;
-        default:
-            console.log(
-                'Você tentou um número ou caractere diferente, né? Espertinho.'
-            );
-            break;
+        if (!isNaN(number) && number >= 1 && number <= 3 && Number.isInteger(number)) {
+            return number
+        }
+        console.log('\nPor favor, digite um número entre 1 e 3.')
     }
 }
 
-veryUsefulSystem();
+function requestUserInput() {
+    console.log('Bem-vindo ao sistema que não faz nada, escolha sua opção de forma sábia!\n')
+    console.log(
+        "[1] Não fazer nada e fechar\n[2] Imprimir um número aleatório de 0 a 10\n[3] Ler 'Obrigado por usar o sistema' e encerrar a execução"
+    )
+
+    const option = getValidnumber('Escolha sua opção: ')
+
+    return option
+}
+
+function veryUsefulSystem(option) {
+    switch (option) {
+        case 1:
+            console.log('Ok... Tchau!')
+            process.exit()
+        case 2:
+            const number = Math.floor(Math.random() * 11)
+            if (number === 0) {
+                return `Ainda é azarado bicho, tu tirou ${number}!!!!`
+            } else {
+                return number
+            }
+        case 3:
+            return 'Obrigado por usar o sistema!'
+    }
+}
+
+function main() {
+    let continueProgram = true
+
+    while (continueProgram) {
+        const option = requestUserInput()
+        const optionChose = veryUsefulSystem(option)
+
+        console.log(optionChose)
+
+        console.log('\nDeseja usar o menu novamente? \n')
+
+        continueProgram = shouldContinue()
+    }
+}
+
+main()

849ca6c1de3c95fd5b9d461c6aee0052d3f9dc47
tenchini <tenchini.dev@gmail.com>
2025-05-08 17:02:41 -0300

    refactor: refatora ex01 delegando funções e evitando repetição

--- ex01.js
+++ ex01.js
@@ -1,18 +1,45 @@
 // Escreva um programa que recebe um número inteiro e verifica se ele é par ou ímpar
 // utilizando uma estrutura de controle if.
 
-function isEvenOrOdd(number) {
-    if (isNaN(number) || !Number.isInteger(number)) {
-        console.log('Por favor, digite um número inteiro válido.');
-        return;
+const prompt = require('prompt-sync')()
+
+const { shouldContinue } = require('./askToContinue.js')
+
+function getValidNumber(message) {
+    while (true) {
+        const input = prompt(message)
+        const number = Number(input)
+
+        if (!isNaN(number) && Number.isInteger(number)) {
+            return number
+        }
+        console.log('\nPor favor, digite um número inteiro válido.')
     }
-    number % 2 === 0
-        ? console.log(`O número ${number} é par!`)
-        : console.log(`O número ${number} é ímpar!`);
 }
 
-isEvenOrOdd(14);
-isEvenOrOdd(7);
-isEvenOrOdd('a');
-isEvenOrOdd(1.3);
-isEvenOrOdd(-2);
+function requestUserInput() {
+    const number = getValidNumber('Digite um número inteiro: ')
+
+    return number
+}
+
+function isEvenOrOdd(number) {
+    number % 2 === 0
+        ? console.log(`\nO número ${number} é par!`)
+        : console.log(`\nO número ${number} é ímpar!`)
+}
+
+function main() {
+    let continueProgram = true
+
+    while (continueProgram) {
+        const number = requestUserInput()
+        isEvenOrOdd(number)
+
+        console.log('\nDeseja saber outro número?')
+
+        continueProgram = shouldContinue()
+    }
+}
+
+main()

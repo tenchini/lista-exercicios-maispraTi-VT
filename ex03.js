c07b3e5aa21a64e279d6437e7ae0fa599c6d5297
tenchini <tenchini.dev@gmail.com>
2025-05-08 17:03:23 -0300

    refactor: refatora ex03 delegando funções e evitando repetição

--- ex03.js
+++ ex03.js
@@ -1,25 +1,51 @@
 // Implemente um programa que recebe uma nota de 0 a 10 e classifica como
 // "Aprovado", "Recuperação", ou "Reprovado" utilizando if-else if.
 
-function getStudentStatus(nota) {
-    if (isNaN(nota) || nota > 10 || nota < 0) {
-        console.log(
-            'Por favor, verifique a nota do aluno. A nota precisa ser um número positivo.'
-        );
-        return;
-    }
-    if (nota >= 7) {
-        console.log('Aprovado');
-    } else if (nota >= 5) {
-        console.log('Recuperação');
-    } else {
-        console.log('Reprovado');
+const prompt = require('prompt-sync')()
+
+const { shouldContinue } = require('./askToContinue.js')
+
+function getValidGrade(message) {
+    while (true) {
+        const input = prompt(message)
+        const grade = parseFloat(input)
+
+        if (!isNaN(grade) && grade >= 0 && grade <= 10) {
+            return grade
+        }
+        console.log('\nPor favor, digite uma nota válida.')
     }
 }
 
-getStudentStatus(3.5);
-getStudentStatus(6.7);
-getStudentStatus(9.8);
-getStudentStatus('a');
-getStudentStatus(-1);
-getStudentStatus(11);
+function requestUserInput() {
+    const grade = getValidGrade('Digite a nota do aluno: ')
+
+    return grade
+}
+
+function getStudentStatus(grade) {
+    if (grade >= 7) {
+        return '\nAprovado'
+    } else if (grade >= 5) {
+        return '\nRecuperação'
+    } else {
+        return '\nReprovado'
+    }
+}
+
+function main() {
+    let continueProgram = true
+
+    while (continueProgram) {
+        const grade = requestUserInput()
+        const gradeStatus = getStudentStatus(grade)
+
+        console.log(gradeStatus)
+
+        console.log('\nDeseja consultar outra nota? ')
+
+        continueProgram = shouldContinue()
+    }
+}
+
+main()

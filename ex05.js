bb7e417112d2a076c27d6c3daf7292d40505f37b
tenchini <tenchini.dev@gmail.com>
2025-05-08 17:03:48 -0300

    refactor: refatora ex05 delegando funções e evitando repetição

--- ex05.js
+++ ex05.js
@@ -1,39 +1,81 @@
 // Escreva um programa que calcula o Índice de Massa Corporal (IMC) de uma pessoa e
-// determina a categoria de peso (baixo peso, peso normal, sobrepeso, obesidade)
+// determina a categoria de result (baixo result, result normal, sobrepeso, obesidade)
 // utilizando if-else.
 
-const prompt = require('prompt-sync')();
+const prompt = require('prompt-sync')()
 
-function calculateIMC() {
-    let peso;
+const { shouldContinue } = require('./askToContinue.js')
+
+function getValidWeight(message) {
     while (true) {
-        let entrada1 = prompt('Qual seu peso? ').replace(',', '.');
-        peso = parseFloat(entrada1);
+        const input = prompt(message)
+        const weight = parseFloat(input)
 
-        if (isNaN(peso)) {
-            console.log('Peso inválido.');
-        } else {
-            break;
+        if (!isNaN(weight) && weight >= 30 && weight <= 500) {
+            return weight
         }
+        console.log('\nPeso inválido! Tente novamente.')
     }
-
-    let altura;
-    while (true) {
-        let entrada2 = prompt('Qual sua altura? ').replace(',', '.');
-        altura = parseFloat(entrada2);
-
-        if (isNaN(altura)) {
-            console.log('Altura inválida.');
-        } else {
-            if (!entrada2.includes('.')) {
-                altura /= 100;
-            }
-            break;
-        }
-    }
-
-    let calculoIMC = (peso / (altura * altura)).toFixed(2);
-    console.log(`Seu IMC é: ${calculoIMC}`);
 }
 
-calculateIMC();
+function getValidHeight(message) {
+    while (true) {
+        const input = prompt(message).replace(',', '.')
+        let height = parseFloat(input)
+
+        if (!isNaN(height)) {
+            if (height > 10) {
+                height /= 100
+            }
+            if (height >= 0.6 && height <= 2.51) {
+                return height
+            }
+        }
+        console.log('\nAltura inválida! Tente novamente.')
+    }
+}
+
+function requestUserHeight() {
+    const height = getValidHeight('Digite sua altura: ')
+    return height
+}
+
+function requestUserWeight() {
+    const weight = getValidWeight('Digite seu peso: ')
+    return weight
+}
+
+function calculateIMC(weight, height) {
+    const calculateBMI = weight / (height * height)
+    return calculateBMI.toFixed(2)
+}
+
+function getCategory(calculateBMI) {
+    if (calculateBMI < 18.5) {
+        return 'Baixo peso.'
+    } else if (calculateBMI < 25) {
+        return 'Peso adequado.'
+    } else if (calculateBMI < 35) {
+        return 'Sobrepeso.'
+    } else {
+        return 'Obesidade'
+    }
+}
+
+function main() {
+    let continueProgram = true
+
+    while (continueProgram) {
+        const userHeight = requestUserHeight()
+        const userWeight = requestUserWeight()
+
+        const userBMI = calculateIMC(userWeight, userHeight)
+        const userCategory = getCategory(userBMI)
+
+        console.log(`\nSeu IMC é: ${userBMI}, ${userCategory}\n`)
+
+        continueProgram = shouldContinue()
+    }
+}
+
+main()
